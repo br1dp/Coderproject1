3997692aa7afdd87aedfb4d35d13603f3ec1a42b
Bruno DP <brunoen99@gmail.com>
2022-07-24 23:08:22 -0300

    Cosas agregadas sobre la url admin y su potencial

--- app_coder/admin.py
+++ app_coder/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from app_coder.models import Curso, Entregable, Estudiantes, Profesor
+
+class ProfesorAdmin(admin.ModelAdmin):
+
+    list_display = ("nombre","apellido","profesion","email")
+    search_fields = ("nombre","apellido")
+
+
+admin.site.register(Curso)
+admin.site.register(Estudiantes)
+admin.site.register(Profesor,ProfesorAdmin)
+admin.site.register(Entregable)
--- app_coder/models.py
+++ app_coder/models.py
@@ -8,11 +8,23 @@ class Curso(models.Model):
     nombre = models.CharField(max_length=50)
     camada = models.IntegerField()
 
+    def __str__(self) -> str:
+        return f"{self.nombre} - {self.camada}"
+
+    class Meta():
+        verbose_name = "My Course" 
+        verbose_name_plural = "Mis Cursos"
+        ordering = ("nombre",)
+        unique_together = ("nombre","camada")
+
 class Estudiantes(models.Model):
 
     nombre = models.CharField(max_length=50)
     apellido = models.CharField(max_length=50)
     email = models.EmailField(max_length=254)
+
+    def __str__(self) -> str:
+        return f"{self.nombre} - {self.apellido}"
 
 class Profesor(models.Model):
     nombre = models.CharField(max_length=50)
@@ -20,7 +32,14 @@ class Profesor(models.Model):
     email = models.EmailField( max_length=254)
     profesion = models.CharField( max_length=50)
 
+    def __str__(self) -> str:
+        return f"{self.nombre} - {self.apellido} - {self.profesion} - {self.email}"
+
 class Entregable(models.Model):
     nombre = models.CharField(max_length=50)
     FechaDeEntrega = models.DateField()
     entregado = models.BooleanField()
+    estudiante = models.ForeignKey(Estudiantes,on_delete=models.CASCADE,default=1)
+
+    def __str__(self) -> str:
+        return f"{self.nombre} - {self.FechaDeEntrega}"
